67bf1df7b61afd6f483a951da1bd03b55f2378f3
Dan Palmer <dan.palmer@me.com>
2014-12-24 17:52:24 +0000

    Fix indentation

--- django_google_charts/static/django_google_charts/charts.js
+++ django_google_charts/static/django_google_charts/charts.js
@@ -13,34 +13,34 @@ $(document).ready(function() {
     var dataText = $('#' + chartId).text();
     var data = JSON.parse(dataText);
 
-      var dataTable = new google.visualization.DataTable();
+    var dataTable = new google.visualization.DataTable();
 
-      var types = [];
+    var types = [];
 
-      for (var i = 0; i < data.cols.length; i++) {
-        var col = data.cols[i];
-        types.push(col[0]);
-        dataTable.addColumn(col[0], col[1]);
-      }
+    for (var i = 0; i < data.cols.length; i++) {
+      var col = data.cols[i];
+      types.push(col[0]);
+      dataTable.addColumn(col[0], col[1]);
+    }
 
-      for (var i = 0; i < data.rows.length; i++) {
-        var row = data.rows[i];
-        var formattedRow = [];
-        for (var j = 0; j < row.length; j++) {
-          switch (types[j]) {
-            case 'datetime':
-              formattedRow.push(new Date(row[j]));
-              break;
-            default:
-              formattedRow.push(row[j]);
-          }
+    for (var i = 0; i < data.rows.length; i++) {
+      var row = data.rows[i];
+      var formattedRow = [];
+      for (var j = 0; j < row.length; j++) {
+        switch (types[j]) {
+          case 'datetime':
+            formattedRow.push(new Date(row[j]));
+            break;
+          default:
+            formattedRow.push(row[j]);
         }
-
-        dataTable.addRow(formattedRow);
       }
 
-      var chart = new google.visualization.LineChart(chartElement);
-      chart.draw(dataTable, options);
+      dataTable.addRow(formattedRow);
+    }
+
+    var chart = new google.visualization.LineChart(chartElement);
+    chart.draw(dataTable, options);
   }
 
   google.setOnLoadCallback(drawCharts);
