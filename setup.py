bdecb849ddafc0ed57db1758e7a4eda824bfa2ad
Dan Palmer <dan.palmer@me.com>
2014-12-29 15:18:01 +0000

    First pass at setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="django-google-charts",
+    version="0.1.0",
+    author="Dan Palmer",
+    author_email="dan@danpalmer.me",
+    packages=["django_google_charts"],
+    url="http://danpalmer.me/projects/django-google-charts",
+    description="A simple Python interface to Google Charts.",
+)
