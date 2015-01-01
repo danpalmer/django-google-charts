9adc8106109f23ebf53a96b06e12022a01e8e106
Dan Palmer <dan.palmer@me.com>
2015-01-01 15:04:43 +0000

    Single quotes for non-human readable strings

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     author="Dan Palmer",
     author_email="dan@danpalmer.me",
     packages=[
-        "django_google_charts",
-        "django_google_charts.templatetags",
+        'django_google_charts',
+        'django_google_charts.templatetags',
     ],
-    url="http://danpalmer.me/projects/django-google-charts",
+    url='http://danpalmer.me/projects/django-google-charts',
     description="A simple Python interface to Google Charts.",
 )
