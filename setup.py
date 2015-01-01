5192076ca066e1a602d1260c75a534afac00275e
Dan Palmer <dan.palmer@me.com>
2015-01-01 15:04:07 +0000

    Include template tags package

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version="0.1.0",
     author="Dan Palmer",
     author_email="dan@danpalmer.me",
-    packages=["django_google_charts"],
+    packages=[
+        "django_google_charts",
+        "django_google_charts.templatetags",
+    ],
     url="http://danpalmer.me/projects/django-google-charts",
     description="A simple Python interface to Google Charts.",
 )
