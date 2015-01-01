8cf0e51aa6dd38e335c122987fcd24107a06989e
Dan Palmer <dan.palmer@me.com>
2015-01-01 15:07:15 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="django-google-charts",
-    version="0.1.0",
+    version="0.1.1",
     author="Dan Palmer",
     author_email="dan@danpalmer.me",
     packages=[
