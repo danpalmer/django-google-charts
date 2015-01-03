5a4a9e5cc2a8ac74fbbb2139c059b5b32c34968c
Dan Palmer <dan.palmer@me.com>
2015-01-03 16:48:54 +0000

    No trailing slash

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="django-google-charts",
-    version="0.1.4",
+    version="0.1.5",
     author="Dan Palmer",
     author_email="dan@danpalmer.me",
     packages=[
