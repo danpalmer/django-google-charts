2a26daceec59ad7ad3238c997e1bfc167c74ba93
Dan Palmer <dan.palmer@me.com>
2015-01-03 16:56:20 +0000

    Maybe package data is needed?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="django-google-charts",
-    version="0.1.5",
+    version="0.1.6",
     author="Dan Palmer",
     author_email="dan@danpalmer.me",
     packages=[
@@ -11,4 +11,5 @@ setup(
     ],
     url='http://danpalmer.me/projects/django-google-charts',
     description="A simple Python interface to Google Charts.",
+    include_package_data=True,
 )
