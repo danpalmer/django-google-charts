99e509a4a99d48115c13a90ab1aabbdc7304c011
Dan Palmer <dan.palmer@me.com>
2015-01-11 17:53:19 +0000

    Remove classes for pytest

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,65 +1,65 @@
-import unittest
-
 from django_google_charts.utils import OptionsDict
 
 
-class OptionsTests(unittest.TestCase):
+def test_dot_access(self):
+    key, value = 'key', 'value'
+    options = OptionsDict({key: value})
 
-    def test_dot_access(self):
-        key, value = 'key', 'value'
-        options = OptionsDict({key: value})
+    self.assertEqual(options.key, value)
 
-        self.assertEqual(options.key, value)
+    options2 = OptionsDict()
+    options2.key = value
 
-        options2 = OptionsDict()
-        options2.key = value
+    self.assertEqual(options2.key, value)
 
-        self.assertEqual(options2.key, value)
 
-    def test_options_equality(self):
-        options = OptionsDict({'key1': 'value', 'key2': 3})
-        test = {'key1': 'value', 'key2': 3}
+def test_options_equality(self):
+    options = OptionsDict({'key1': 'value', 'key2': 3})
+    test = {'key1': 'value', 'key2': 3}
 
-        self.assertEqual(options, test)
+    self.assertEqual(options, test)
 
-    def test_adding_dict(self):
-        value = 'value'
-        options = OptionsDict()
 
-        options.x = {}
-        options.x.y = value
+def test_adding_dict(self):
+    value = 'value'
+    options = OptionsDict()
 
-        self.assertEqual(options.x.y, value)
+    options.x = {}
+    options.x.y = value
 
-    def test_options_merge(self):
-        options = OptionsDict({'overridden': 1, 'kept': 1})
-        suboptions = OptionsDict({'overridden': 2, 'new': 1})
+    self.assertEqual(options.x.y, value)
 
-        options.merge(suboptions)
 
-        self.assertEqual(options, {
-            'overridden': 2,
-            'new': 1,
+def test_options_merge(self):
+    options = OptionsDict({'overridden': 1, 'kept': 1})
+    suboptions = OptionsDict({'overridden': 2, 'new': 1})
+
+    options.merge(suboptions)
+
+    self.assertEqual(options, {
+        'overridden': 2,
+        'new': 1,
+        'kept': 1,
+    })
+
+
+def test_options_recursive_merge(self):
+    options = OptionsDict({
+        'key': {
+            'subkey': {'subsubkey': 1},
             'kept': 1,
-        })
+        },
+    })
 
-    def test_options_recursive_merge(self):
-        options = OptionsDict({
-            'key': {
-                'subkey': {'subsubkey': 1},
-                'kept': 1,
-            },
-        })
+    options.merge(OptionsDict({
+        'key': {
+            'subkey': {'subsubkey': 2},
+        },
+    }))
 
-        options.merge(OptionsDict({
-            'key': {
-                'subkey': {'subsubkey': 2},
-            },
-        }))
-
-        self.assertEqual(options, {
-            'key': {
-                'subkey': {'subsubkey': 2},
-                'kept': 1,
-            },
-        })
+    self.assertEqual(options, {
+        'key': {
+            'subkey': {'subsubkey': 2},
+            'kept': 1,
+        },
+    })
--- tests/test_template_tags.py
+++ tests/test_template_tags.py
@@ -1,9 +1,5 @@
-import unittest
-
 from django_google_charts.templatetags import django_google_charts
 
 
-class TemplateTagTests(unittest.TestCase):
-
-    def test_template_tag_resources(self):
-        html = django_google_charts.django_google_chart_js()
+def test_template_tag_resources(self):
+    html = django_google_charts.django_google_chart_js()
