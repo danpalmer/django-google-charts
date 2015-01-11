6e6b1de6c89fb7c8075cecc2b1027328731b2067
Dan Palmer <dan.palmer@me.com>
2015-01-11 18:37:30 +0000

    derp, self

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,49 +1,49 @@
 from django_google_charts.utils import OptionsDict
 
 
-def test_dot_access(self):
+def test_dot_access():
     key, value = 'key', 'value'
     options = OptionsDict({key: value})
 
-    self.assertEqual(options.key, value)
+    assert options.key == value
 
     options2 = OptionsDict()
     options2.key = value
 
-    self.assertEqual(options2.key, value)
+    assert options2.key == value
 
 
-def test_options_equality(self):
+def test_options_equality():
     options = OptionsDict({'key1': 'value', 'key2': 3})
     test = {'key1': 'value', 'key2': 3}
 
-    self.assertEqual(options, test)
+    assert options == test
 
 
-def test_adding_dict(self):
+def test_adding_dict():
     value = 'value'
     options = OptionsDict()
 
     options.x = {}
     options.x.y = value
 
-    self.assertEqual(options.x.y, value)
+    assert options.x.y == value
 
 
-def test_options_merge(self):
+def test_options_merge():
     options = OptionsDict({'overridden': 1, 'kept': 1})
     suboptions = OptionsDict({'overridden': 2, 'new': 1})
 
     options.merge(suboptions)
 
-    self.assertEqual(options, {
+    assert options == {
         'overridden': 2,
         'new': 1,
         'kept': 1,
-    })
+    }
 
 
-def test_options_recursive_merge(self):
+def test_options_recursive_merge():
     options = OptionsDict({
         'key': {
             'subkey': {'subsubkey': 1},
@@ -57,9 +57,9 @@ def test_options_recursive_merge(self):
         },
     }))
 
-    self.assertEqual(options, {
+    assert options == {
         'key': {
             'subkey': {'subsubkey': 2},
             'kept': 1,
         },
-    })
+    }
--- tests/test_template_tags.py
+++ tests/test_template_tags.py
@@ -1,5 +1,7 @@
+from django.utils.safestring import SafeString
 from django_google_charts.templatetags import django_google_charts
 
 
-def test_template_tag_resources(self):
+def test_template_tag_resources():
     html = django_google_charts.django_google_chart_js()
+    assert isinstance(html, SafeString)
