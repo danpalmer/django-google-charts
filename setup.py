ef02975abefbf1e63ec13fe0454b5e92ce947468
Dan Palmer <dan.palmer@me.com>
2015-01-11 17:50:34 +0000

    Use setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name="django-google-charts",
     version="0.1.6",
     author="Dan Palmer",
     author_email="dan@danpalmer.me",
-    packages=[
-        'django_google_charts',
-        'django_google_charts.templatetags',
-    ],
+    packages=find_packages(exclude=['tests']),
     url='http://danpalmer.me/projects/django-google-charts',
     description="A simple Python interface to Google Charts.",
     include_package_data=True,
